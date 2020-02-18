417de13e622f084a80d8dcd205c404f16671f03b
Sergiu C._ <sergiu.cheregi@yahoo.com>
2020-02-18 21:41:00 +0200

    done

---
+++ ESportTeamMgmtAPp/MyDoubleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ESportTeamMgmtAPp
+{
+    class MyDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double v = (double)value;
+            return (int)v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
